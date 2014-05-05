d4cfd75e9e47ae692cdbfb67e3278af75180fd6d
Auðun Velte Atlason <Skuggomann+Git@Gmail.com>
2014-05-05 16:08:20 +0000

    Default fonts and font objects

--- main.lua
+++ main.lua
@@ -13,4 +13,10 @@ require 'levelselect'
 function love.load()
     Gamestate.registerEvents()
 	Gamestate.switch(Gamestate.menu)
+	
+	--Loading fonts
+	Font12p = love.graphics.newFont("/assets/fonts/visitor1.ttf", 12)
+	Font18p = love.graphics.setNewFont("/assets/fonts/visitor1.ttf", 18)
+	Font24p = love.graphics.newFont("/assets/fonts/visitor1.ttf", 24)
+	Font36p = love.graphics.newFont("/assets/fonts/visitor1.ttf", 36)
 end
