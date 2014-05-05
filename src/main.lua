21ffb61e9d502c94f937f6044adbf74c8dd6bb40
Snorri Arnarson <snorriarn@gmail.com>
2014-05-05 00:44:17 +0000

    libraries

---
+++ src/main.lua
@@ -0,0 +1,5 @@
+loader = require "libs.Advanced-Tiled-Loader.Loader"
+Gamestate = require "libs.hump.gamestate"
+Class = require "libs.hump.class"
+Camera = require "libs.hump.camera"
+HC = require 'libs.HardonCollider'
