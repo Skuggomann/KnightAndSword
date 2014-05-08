6dfb1d61372dbf7f2b0e8af927ce1f57dc34f6db
Auðun Velte Atlason <Skuggomann+Git@Gmail.com>
2014-05-08 12:12:04 +0000

    Updated controlls

--- player.lua
+++ player.lua
@@ -27,15 +27,15 @@ Player = Class{
 }
 function Player:update(dt)
 	-- update controls
-    if love.keyboard.isDown("left") or controls:isDown(2, "left") then
+    if controls:isDown("left") then
         self.velocity.x = -self.speed*dt
         self.facingRight = false
     end
-    if love.keyboard.isDown("right") or controls:isDown(2, "right") then
+    if controls:isDown("right") then
         self.velocity.x = self.speed*dt
         self.facingRight = true
     end
-    if (love.keyboard.isDown("up") or controls:isDown(2, "up")) and not self.jumping then
+    if controls:isDown("up") and not self.jumping then
 
         self.velocity.y = -1000*dt
     	self.jumping = true
@@ -46,7 +46,7 @@ function Player:update(dt)
     if self.jumping then
     	self.velocity.y = self.velocity.y + self.speed/4*dt
     end
-    if (love.keyboard.isDown(" ") or controls:isDown(2, "x")) and self.sword:canAttack() then
+    if controls:isDown("attack") and self.sword:canAttack() then
     	self.sword:attack()
     end
     -- update movement
@@ -60,7 +60,7 @@ function Player:update(dt)
     end
     self.sword:update(dt,x,y)
 
-    if love.keyboard.isDown("w") then
+    if controls:isDown("cast") then
         if self.currentAbility == "frostbolt" then
             if self.frostbolt.cooldown == 0 and self.mana >= self.frostbolt.manacost then
                 self.frostbolt:addBolt()
