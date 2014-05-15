47f02c5ef7d2d49297cb2fffde5111814439d0e3
Snorri Arnarson <snorriarn@gmail.com>
2014-05-15 13:19:29 +0000

    lolololol conflicts

--- player.lua
+++ player.lua
@@ -25,7 +25,11 @@ Player = Class{
 	    self.MAXINVULN = 1
 		self.sprite = love.graphics.newImage('/assets/art/player2.png')
 		self.spriteJumping = love.graphics.newImage('/assets/art/player2jumping.png')
-		self.spriteWalkin = love.graphics.newImage('/assets/art/icecube.png')
+        self.spriteWalkin = love.graphics.newImage('/assets/art/player2short.png')
+        local grid = anim8.newGrid(32, 64, self.spriteWalkin:getWidth(), self.spriteWalkin:getHeight(), 0, 0, 0)
+        self.animationWalkinR = anim8.newAnimation(grid('1-4',1), 0.3)
+        self.animationWalkinL = anim8.newAnimation(grid('1-4',1), 0.3):flipH()
+
 		--self.sword = Sword(x,y,collider, self)
         --self.frostbolt = Frostbolt(collider, self)
 
@@ -80,7 +84,10 @@ function Player:update(dt)
 
     self.abilities["frostbolt"]:update(dt)
     self.abilities["telekinesis"]:update(dt)
-    --self.frostbolt:update(dt)
+    -- update animations
+    self.animationWalkinR:update(dt)
+    self.animationWalkinL:update(dt)
+
 end
 
 function Player:attack()
@@ -111,7 +118,7 @@ function Player:draw()
 	if(self.jumping) then
 		currentSprite = self.spriteJumping
 	elseif (controls:isDown("right") or controls:isDown("left")) and not self.jumping then	
-		currentSprite = self.spriteWalkin
+		currentSprite = nil
 	else
 		currentSprite = self.sprite
 	end
@@ -120,11 +127,20 @@ function Player:draw()
 	
 	
 	
-    if self.facingRight then
-    	love.graphics.draw(currentSprite, x - 16, y - 37)
+    if currentSprite == nil then
+        if self.facingRight then
+            self.animationWalkinR:draw(self.spriteWalkin, x - 16, y - 37)
+        else
+            self.animationWalkinL:draw(self.spriteWalkin, x - 16, y - 37)
+        end
     else
-        love.graphics.draw(currentSprite, x + 16, y - 37, 0, -1, 1)
+        if self.facingRight then
+            love.graphics.draw(currentSprite, x - 16, y - 37)
+        else
+            love.graphics.draw(currentSprite, x + 16, y - 37, 0, -1, 1)
+        end
     end
+
 
 	-- draw weapon... (just sword now)
 	self.weapons[self.currentWeapon]:draw()
@@ -134,6 +150,11 @@ function Player:takeDamage(damage)
 	self.hp = self.hp-damage
 	self.invuln = self.MAXINVULN
 end
+function Player:recoverHealth(health)
+    self.hp = self.hp+health
+    if self.hp > self.maxhp then self.hp = self.maxhp end
+end
+
 function Player:isInvuln()
 	return self.invuln > 0
 end
@@ -300,6 +321,9 @@ function Player:collide(dt, me, other, mtv_x, mtv_y)
 			-- collision with frozen skeleton same as tile(ground)
 			self:collisionWithSolid(mtv_x,mtv_y)
 		end
+    elseif other.type = "healthvial" then
+        self:recoverHealth(1)
+        other.ref.pickedUp = true
     elseif other.type =="TheVeil" then
         self:takeDamage(100)
 	elseif other.type == "end" then
