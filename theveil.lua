f26291a779b4b8cf19693dc04335aed28b1be5f2
ThorarinnS <thorarinn.sveinsson@gmail.com>
2014-05-16 13:02:38 +0000

    tentacles

--- theveil.lua
+++ theveil.lua
@@ -1,11 +1,12 @@
 TheVeil = Class{
     init = function(self, x, collider)
-        self.bbox = collider:addRectangle(x,0,-128,H)
+        self.bbox = collider:addRectangle(x-75,0,75,H)
         self.bbox.type = "TheVeil"
         self.bbox.ref = self
         self.velocityX = 60
         self.collider = collider
-		--self.sprite = love.graphics.newImage('/assets/art/.png')
+		self.sprite = love.graphics.newImage('/assets/art/VeilOfSOUUUUUULS.png')
+        self.sprite:setWrap("clamp","repeat")
 
     end
 }
@@ -17,10 +18,23 @@ function TheVeil:update(dt)
 end
 
 function TheVeil:draw()
-    love.graphics.setColor(255,0,0, 192)
-    self.bbox:draw("fill")
+    local x1,y1, x2,y2 = self.bbox:bbox()
+    print(x2 .. " " .. y2)
+    local quad = love.graphics.newQuad(0,0,75,H,75,128)
+    love.graphics.draw(self.sprite, quad,x1,y1)
+    if debug then
+        love.graphics.setColor(255,0,0, 192)
+        self.bbox:draw("fill")
+        love.graphics.setColor(255,255,255, 255)
+    end
+    love.graphics.setColor(0,0,0, 255)
+    love.graphics.rectangle("fill",x1,y1,-W,H)
+
+    --self.bbox:draw("fill")
     love.graphics.setColor(255,255,255, 255)
-    local x,y = self.bbox:center()
+
+
+    --local x,y = self.bbox:center()
     --love.graphics.draw(self.sprite, x - 16, y - 32)
 
 
