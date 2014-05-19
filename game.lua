6a2d50a3c8a550d539ed2ac6fb86f221352de7ac
ThorarinnS <thorarinn.sveinsson@gmail.com>
2014-05-19 11:49:49 +0000

    removed fps draw

--- game.lua
+++ game.lua
@@ -240,7 +240,7 @@ end
 function game:draw()
 	cam:draw(drawWorld)
 	ui:draw()
-   	love.graphics.print("Current FPS: "..tostring(love.timer.getFPS( )), 10, 10)
+   	--love.graphics.print("Current FPS: "..tostring(love.timer.getFPS( )), 10, 10)
 end
 
 
